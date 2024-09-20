22abf561dbf6477b0c8ff879dc2bfb775722fb33
x9laaa <acarreno95@outlook.com>
2024-09-20 08:51:21 -0300

    actividad 12 terminada

---
+++ app/src/main/java/cl/bootcamp/ind_12/FirstFragment.java
@@ -0,0 +1,35 @@
+package cl.bootcamp.ind_12;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.bootcamp.ind_12.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnComenzar.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_firstFragment_to_secondFragment);
+        });
+
+    }
+}
